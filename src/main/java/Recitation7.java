a76d321dd8a201b2722cb6bf782eaef7f11c8c2a
twashington04 <torrencetyshon@gmail.com>
2023-03-31 22:08:07 -0400

    Recitation7 completed

--- src/main/java/Recitation7.java
+++ src/main/java/Recitation7.java
@@ -9,26 +9,44 @@
 public class Recitation7
 {
     // define your constants after this comment
+    private static String replicate(char ch, int ntimes){
+        String str = "";
+        for(int i = 0; i < ntimes; i++){
+            str += ch;
+        }
+        return str;
+    }
 
 
-    public static String leftRightTriangle(int height)
+    public static String leftRightTriangle( int height)
     {
         String str = "";
-        //add your code here
+        for(int row = 0; row < height; row++){
+            str += replicate('*', row+1);
+            str += "\n";
+        }
         return str;
     }
 
     public static String rightRightTriangle(int height)
     {
         String str = "";
-        //add your code here
+        for(int row = 0; row < height; row++){
+            str += replicate(' ', height-(row+1));
+            str += replicate('*', row+1);
+            str += "\n";
+        }
         return str;
     }
 
     public static String circle(int radius)
     {
         String str = "";
-        //add your code here
+        int diameter = radius*2;
+        for (int y = radius - 1; y > -radius; y--) {
+          int x = (int)Math.sqrt(radius*radius - y*y);
+          str += replicate(' ', radius - x) + replicate('*',2*x)+"\n";
+            }
         return str;
-    }
+        }
 }
